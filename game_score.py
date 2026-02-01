3bf6247947934d99a17a136f2cc1c52aac404fb5
Ramcharan1298 <yachamaneniramcharan@gmail.com>
2026-02-01 10:03:56 +0530

    game_score.py is added for assignment

---
+++ game_score.py
@@ -0,0 +1,7 @@
+player = str(input("Enter the player name : "))
+games_played = int(input("Enter no.of games played : "))
+Total_score = int(input("Enter the total score of the game : "))
+
+Average_score = Total_score / games_played
+
+print(Average_score) 
